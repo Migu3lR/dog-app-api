faa317ddb0346cfd056347c66c5e661ae1603bc1
Miguel Romero <migu3lr@localhost.localdomain>
2018-08-11 18:26:13 -0500

    pruebas redux

--- redux.js
+++ redux.js
@@ -1,22 +1,38 @@
 import iot from './libs/iotPublisher'
 
+const getParams = (cid, action, payload, qos = 0) => {
+    return {
+        topic: `/redux/${cid}/${action}`,
+        payload: JSON.stringify(payload),
+        qos
+    }
+}
+
 export function handler(event, context, callback) {
     
-    const params = {
-        topic: `/redux/${event.cid}`,
-        payload: JSON.stringify(event),
-        qos: 0
-    };
+    const {action} = event;
 
-    iot.publish(params, function(err, data){
-        if(err){
-            console.log(err);
-            callback(err);
-        }
-        else{
-            console.log("success?");
-            callback(null, event);
-            //context.succeed(event);
-        }
-    });
+    console.log(action);
+
+    switch (action.type) {
+        case 'AUTH_VALIDATION':
+            iot.publish(getParams(event.cid, 'authValidated', {
+                validation: true
+            }), function(err, data){
+                if(err){
+                    console.log(err);
+                    callback(err);
+                }
+                else{
+                    console.log("success?");
+                    callback(null, event);
+                    //context.succeed(event);
+                }
+            });
+        
+        default:
+            console.log('default...');
+    }
+
+    
 }
